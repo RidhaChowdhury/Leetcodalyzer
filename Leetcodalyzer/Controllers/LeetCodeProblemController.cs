7fad767dd5b793a70b88ed079340320f9d872725
Ridha Chowdhury <chowdhuryridha@gmail.com>
2023-01-29 22:53:08 -0500

    SQL query cleaned up, logs removed, and full LeetCodeProblem data supported

--- Leetcodalyzer/Controllers/LeetCodeProblemController.cs
+++ Leetcodalyzer/Controllers/LeetCodeProblemController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Npgsql;
+using System.Data;
 
 namespace Leetcodalyzer.Controllers
 {
@@ -17,7 +18,7 @@ namespace Leetcodalyzer.Controllers
         public List<LeetCodeProblem> Get() {
             System.Diagnostics.Debug.WriteLine("Queried the problems");
             // Connecting with the SQL database
-            using (var connection = new NpgsqlConnection(
+            using (NpgsqlConnection connection = new NpgsqlConnection(
                 "Host=ruby.db.elephantsql.com;" +
                 "Username=dqvfqhvx;" +
                 "Password=6Ao49zHAdPUSRZxk0cXSY7-ZHFMr4ytf;" +
@@ -25,20 +26,33 @@ namespace Leetcodalyzer.Controllers
                 connection.Open();
 
                 // Create a command
-                using (var command = new NpgsqlCommand()) {
-                    command.Connection = connection;
-                    command.CommandText = "select * from grind169_raw gr";
+                using (NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM grind169_raw WHERE problem NOT IN (SELECT problem FROM neet307_raw) UNION SELECT * FROM neet307_raw", connection)) {
+                    /*NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(command);
+                    DataSet dataSet = new DataSet();
 
+                    adapter.Fill(dataSet);
+                    List<LeetCodeProblem> problemList = new List<LeetCodeProblem>();
+                    DataTable table = dataSet.Tables[0];
+
+                    foreach (DataRow row in table.Rows) {
+                        problemList.Add(new LeetCodeProblem {
+                            name = (string)row["problem"]
+                        });
+                    }
+                    return problemList;*/
                     // Start reading the results of the command
                     using (var reader = command.ExecuteReader()) {
                         // Store the results in a list of LeetCodeProblems
                         var problemList = new List<LeetCodeProblem>();
-
+                        int problemNumber = 0;
                         // Start putting the problems in a list
                         while (reader.Read()) {
-                            Console.Beep();
-                            System.Diagnostics.Debug.WriteLine("Grabbing a SQL entry");
-                            problemList.Add(new LeetCodeProblem { name = (string)reader["problem"] });
+                            problemList.Add(new LeetCodeProblem { 
+                                name = (string)reader["problem"],
+                                number = problemNumber++,
+                                difficulty = (string)reader["difficulty"],
+                                category = (string)reader["category"]
+                            });
                         }
                         return problemList;
                     }
--- Leetcodalyzer/LeetCodeProblem.cs
+++ Leetcodalyzer/LeetCodeProblem.cs
@@ -2,6 +2,10 @@ namespace Leetcodalyzer
 {
     public class LeetCodeProblem
     {
+        public int number { get; set; }
         public String name { get; set; }
+
+        public String difficulty { get; set; }
+        public String category { get; set; }
     }
 }
